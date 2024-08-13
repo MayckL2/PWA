74946beb5827a150444d3e390ca8636976d2adf2
mayckl2 <mayckluciano2@gmail.com>
2024-08-13 17:43:01 -0300

    updating manifest

--- manifest.ts
+++ manifest.ts
@@ -1,33 +1,36 @@
 import { VitePWAOptions } from "vite-plugin-pwa";
 
 export const manifestForPlugin: Partial<VitePWAOptions> = {
-registerType: "prompt",
-includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-manifest: {
-        "theme_color": "#8936FF",
-        "background_color": "#2EC6FE",
-        "icons": [
-            {
-                "purpose": "maskable",
-                "sizes": "512x512",
-                "src": "icon512_maskable.png",
-                "type": "image/png"
-            },
-            {
-                "purpose": "any",
-                "sizes": "512x512",
-                "src": "icon512_rounded.png",
-                "type": "image/png"
-            }
-        ],
-        "orientation": "any",
-        "display": "standalone",
-        "lang": "pt-BR",
-        "name": "PWA",
-        "short_name": "pwa",
-        "description": "Aplicação teste PWA",
-        "id": "a2c95dd2-a6c0-4d1f-b32c-f21e6694161e",
-        "scope": "/",
-        "start_url": "https://pwa-tg37.vercel.app/"
-    }
-}
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "PWA",
+    short_name: "pwa",
+    start_url: "https://pwa-tg37.vercel.app/",
+    display: "standalone",
+    background_color: "#2EC6FE",
+    lang: "pt-BR",
+    scope: "/",
+    theme_color: "#8936FF",
+    icons: [
+      {
+        purpose: "maskable",
+        sizes: "512x512",
+        src: "icon512_maskable.png",
+        type: "image/png",
+      },
+      {
+        purpose: "any",
+        sizes: "512x512",
+        src: "icon512_rounded.png",
+        type: "image/png",
+      },
+    ],
+    orientation: "any",
+    description: "Aplicação teste PWA",
+    id: "a2c95dd2-a6c0-4d1f-b32c-f21e6694161e",
+    dir: "ltr",
+    display_override: ["window-controls-overlay"],
+    categories: ["books", "games", "music"],
+  },
+};
